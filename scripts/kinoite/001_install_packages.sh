86e0a75a2a401dc8dd6ff8e3d0b89a97b11a419b
zihad <tazihad@gmail.com>
2024-10-25 10:22:14 +0600

    refactor: :recycle: improve structure

--- scripts/kinoite/001_install_packages.sh
+++ scripts/kinoite/001_install_packages.sh
@@ -2,8 +2,17 @@
 
 set -euox pipefail
 
-rpm-ostree install \
-  qt6-qtimageformats \
-  fcitx5 \
+# Define packages to install
+packages=(
+  qt6-qtimageformats
+  fcitx5
   kcm-fcitx5
+)
 
+# Function to install packages
+install_packages() {
+  rpm-ostree install "${packages[@]}"
+}
+
+# Call the function to install the packages
+install_packages

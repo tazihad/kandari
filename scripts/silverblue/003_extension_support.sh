ef3bdda92345de285d8d10af1cd1616f37d863d6
zihad <tazihad@gmail.com>
2024-10-25 10:31:10 +0600

    feat: :sparkles: legacy theme support

--- scripts/silverblue/003_extension_support.sh
+++ scripts/silverblue/003_extension_support.sh
@@ -8,3 +8,10 @@ gdbus call --session \
            --object-path /org/gnome/Shell/Extensions \
            --method org.gnome.Shell.Extensions.InstallRemoteExtension \
            "appindicatorsupport@rgcjonas.gmail.com"
+
+# Legacy (GTK3) Theme Auto Switcher
+ gdbus call --session \
+           --dest org.gnome.Shell.Extensions \
+           --object-path /org/gnome/Shell/Extensions \
+           --method org.gnome.Shell.Extensions.InstallRemoteExtension \
+           "legacyschemeautoswitcher@joshimukul29.gmail.com"

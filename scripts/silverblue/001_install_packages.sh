fc7c1aa33f3002077f8ba9bd60c686128b68a7cf
zihad <tazihad@gmail.com>
2024-10-25 10:20:40 +0600

    feat: :sparkles: thumbnail for gnome

---
+++ scripts/silverblue/001_install_packages.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euox pipefail
+
+# Define packages to install
+packages=(
+  ffmpegthumbnailer
+)
+
+# Function to install packages
+install_packages() {
+  rpm-ostree install "${packages[@]}"
+}
+
+# Call the function to install the packages
+install_packages
